d48e23eeec07c33236e0eb170878200c695e9450
jck322-gif <jck322@gmail.com>
2026-08-30 23:00:52 +0900

    Add mode prop to Dashboard component in Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import Dashboard from "@/components/Dashboard";
 import { REGIONS } from "@/lib/regions";
 
 export default function Page() {
-  return <Dashboard staticRegions={REGIONS} />;
+  return <Dashboard staticRegions={REGIONS} mode="home" />;
 }
